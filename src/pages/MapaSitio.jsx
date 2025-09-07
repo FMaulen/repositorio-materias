3b25522486c7a0357a40f0788339c4b9f046935e
Jose Barra <jo.barrat@duocuc.cl>
2025-09-07 17:52:47 -0300

    Actualizacion de MapaSitio jsx y css (se agrega el lorem)

--- src/pages/MapaSitio.jsx
+++ src/pages/MapaSitio.jsx
@@ -2,6 +2,10 @@ import { Link } from 'react-router-dom'
 
 import "./MapaSitio.css"
 
+import { Link } from 'react-router-dom'
+
+import "./MapaSitio.css"
+
 function MapaSitio() {
     return (
         <div>
@@ -14,6 +18,26 @@ function MapaSitio() {
                 <li><Link to="/registrarse">Registrarse</Link></li>
                 <li><Link to="/materias">Materias</Link></li>
             </ul>
+            <div className= 'lorem'> 
+
+
+                <h2>Mision</h2>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur laborum fugiat odio, corrupti labore molestiae magni magnam perspiciatis nostrum temporibus sapiente mollitia repellendus beatae quam eos sed dignissimos enim quidem!
+                </p>
+
+                <h2>Vision</h2>
+                <p>
+                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Corrupti enim dolores beatae! Quae provident, doloremque facere, modi sapiente ex vitae accusamus, placeat dolor ratione magni labore laboriosam suscipit id quas.
+                </p>
+
+                <h2>Descripcion</h2>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut sequi, delectus facere, perferendis, dolore non modi consequatur quidem accusamus fugiat mollitia culpa voluptatibus ipsam. Quaerat consequuntur corrupti est possimus placeat.
+                </p>
+
+            </div>
+
         </div>
     );
 }
