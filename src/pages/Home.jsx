91d1317b25ae7529804411955725f0bab7f9b1ef
FMaulen <fabi.maulen@duocuc.cl>
2025-09-07 19:42:09 -0300

    Imagenes home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,12 +2,12 @@ import './Home.css';
 
 function Home() {
     const materias = [
-        { nombre: "FUNDAMENTOS DE PROGRAMACION", codigo: "DSY-1101", archivos: 0 },
-        { nombre: "NIVELACION MATEMATICA", codigo: "MAT-1111", archivos: 47 },
-        { nombre: "BASES DE INNOVACION", codigo: "FPY-1101", archivos: 7 },
-        { nombre: "INTRODUCCION AL CLOUD COMPUTING", codigo: "BIY-1101", archivos: 4 },
-        { nombre: "DESARROLLO ORIENTADO A OBJETOS", codigo: "DSY-1102", archivos: 6 },
-        { nombre: "BASE DE DATOS APLICADA I", codigo: "BDY-1101", archivos: 0 },
+        { nombre: "FUNDAMENTOS DE PROGRAMACION", codigo: "DSY-1101", archivos: 0, img: "../src/assets/python.jpeg" },
+        { nombre: "NIVELACION MATEMATICA", codigo: "MAT-1111", archivos: 47, img: "../src/assets/math.jpg" },
+        { nombre: "BASES DE INNOVACION", codigo: "FPY-1101", archivos: 7, img: "../src/assets/inno.jpg" },
+        { nombre: "INTRODUCCION AL CLOUD COMPUTING", codigo: "BIY-1101", archivos: 4, img: "../src/assets/aws.jpg" },
+        { nombre: "DESARROLLO ORIENTADO A OBJETOS", codigo: "DSY-1102", archivos: 6, img: "../src/assets/java.jpg" },
+        { nombre: "BASE DE DATOS APLICADA I", codigo: "BDY-1101", archivos: 0, img: "../src/assets/oracle.jpg"},
     ];
 
     return (
@@ -20,6 +20,7 @@ function Home() {
             <div className="materias-grid">
                 {materias.map((materia, index) => (
                     <div key={index} className="materia-card">
+                        {materia.img && <img src={materia.img} alt="" className='materia-card-background'/>}
                         <h3>{materia.nombre}</h3>
                         <p className="materia-codigo">{materia.codigo}</p>
                         <span className="materia-archivos">{materia.archivos} archivos</span>
