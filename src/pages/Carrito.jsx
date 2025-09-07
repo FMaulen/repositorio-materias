f7124e842e34098685b3f06a7986c1c0040cd97c
FMaulen <fabi.maulen@duocuc.cl>
2025-09-07 19:00:32 -0300

    Carrito mock

--- src/pages/Carrito.jsx
+++ src/pages/Carrito.jsx
@@ -1,9 +1,18 @@
 import "./Carrito.css"
 
+import { useNavigate } from "react-router-dom"
+
+
 function Carrito() {
+    let navigate = useNavigate();
+    const routeChange = () =>{
+        let path = '/';
+        navigate(path);
+    }
     return (
         <div className="carrito-layout">
             <main className="carrito-container">
+                <h1>Material a descargar</h1>
                 <section className="carrito-grid">
                     <article>
                         <p>Texto 1</p>
@@ -26,7 +35,36 @@ function Carrito() {
                 </section>
             </main >
             <aside className="carrito-aside">
-                <h3>Resumen</h3>
+                <h1>Resumen</h1>
+                <div className="carrito-card">
+                <p className="title-card">Apuntes <br/>ciberseguridad</p>
+                    <p className="date-card">06/08/2025</p>
+                    <p className="price-card">$19.99</p>
+                    <img src="../src/assets/pdf_icon.png" alt="archivo tipo pdf" className="carrito-img"/>
+                </div> 
+                <div className="carrito-card">
+                    <p className="title-card">Apuntes Python</p>
+                    <p className="date-card">04/07/2025</p>
+                    <p className="price-card">$9.99</p>
+                    <img src="../src/assets/pdf_icon.png" alt="archivo tipo pdf" className="carrito-img"/>
+                </div> 
+                <div className="carrito-card">
+                    <p className="title-card">Mer, Mr BD I</p>
+                    <p className="date-card">04/21/2025</p>
+                    <p className="price-card">$4.99</p>
+                    <img src="../src/assets/pdf_icon.png" alt="archivo tipo pdf" className="carrito-img"/>
+                </div> 
+                <div className="carrito-card">
+                    <p className="title-card">Apuntes Java</p>
+                    <p className="date-card">28/03/2025</p>
+                    <p className="price-card">$1.99</p>
+                    <img src="../src/assets/pdf_icon.png" alt="archivo tipo pdf" className="carrito-img"/>
+                </div> 
+                <div className="carrito-total">
+                    <p>Archivos totales: 4</p>
+                    <p>Total: $36.96</p>
+                    <button className="btn-pagar" onClick={routeChange} value={"downloadBtn"}>Pagar y descargar</button>
+                </div>
             </aside>
         </div>
     );
